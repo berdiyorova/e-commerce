693219c8d765035039c01fd3ad7889097b277e39
abrorjonDev <axmadov.git@gmail.com>
2024-06-27 23:07:53 +0500

    AddToCartView ni boshqatdan yozib chiqdim, serializer ni ham. 1. View da faqat business logic bo'lsin: asosiy hisob kitob, tekshiruvlar, serializerlarda yoki model larda, signallarda, service file larda bo'ladi. 2. qilinadigan ishlarni imkon qadar maydalashtirib tashlash kerak. shunda ham readable bo'ladi, ham meros olib ishlatsangiz, override qilish oson., tushunish ham. 3. docstring yozishingiz kerak. nima ish bajarayotgani tushunarli bo'lishi uchun. 4. REST_FRAMEWORK ga asosiy config larni yozing, keyin override qilmasangiz, klass ni o'zida uni qayta yozmang

--- orders/views.py
+++ orders/views.py
@@ -1,36 +1,79 @@
 from rest_framework import status
 from rest_framework.generics import UpdateAPIView, ListAPIView, CreateAPIView
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from products.models import Products
 from orders.serializers import *
 
 
-# Create your views here.
 class AddToCartView(APIView):
     serializer_class = AddToCartSerializer
 
     def post(self, request, *args, **kwargs):
-        try:
-            serializer = self.serializer_class(data=request.data)
-            if not serializer.is_valid():
-                print(serializer.errors)
-                return Response(data={"message": "Invalid data"}, status=status.HTTP_400_BAD_REQUEST)
-            data = serializer.validated_data
-            attributes = data.get('attributes', [])
-            subtotal = sum([AttributeValue.objects.get(id=attribute).price for attribute in attributes])
-            item = CartItem.objects.create(
-                user=request.user,
-                quantity=data.get("quantity"),
-                product=data.get("product"),
-                subtotal=subtotal
-            )
-            return Response(data={"message": "Product added to cart item.", "result": {"cart_item_id": item.id}})
+        serializer = self.get_serializer(data=request.data)
 
-        except Products.DoesNotExist:
-            return Response(data={"message": "Product not found"}, status=status.HTTP_404_NOT_FOUND)
+        # # serializer qaytaradigan hamma exceptionlar
+        # shu yerda clientga qaytariladi.
+        serializer.is_valid(raise_exception=True)
+
+        # # 1. serializer.save() serializer.create() methodga murojaat qiladi
+        # #chunki serializer da hali `instance` attribute o'rnatilmagan,
+        # #aks holda serializer.update(serializer.instance) method ni chaqirgan
+        # #bo'lar edi
+        # # 2. save method ni ichida ba'zi field larni o'rnatib ketish mumkin.:
+        # #     >>> serializer.save(user=request.user)
+        # # attributes va subtotallarni serializer class da hisob kitob qilamiz
+        serializer.save(user=request.user)
+
+        return Response(
+            data={
+                "message": "Product added to cart.",
+                "cart_item_id": serializer.instance.pk
+            }, status=201
+        )
+
+    def get_serializer(self, **kwargs):
+        """
+        Requestda kelgan datani ishlash uchun serializer class ni
+        kerakli parametrlarni o'tkazgan holda qaytaradi.
+        Misol uchun `request` serializer class da ishlatiladimi? shu yerda 
+        qo'shib yuborish kerak 
+        """
+        kwargs = self.get_serializer_context(**kwargs)
+        return self.serializer_class(**kwargs)
+
+    def get_serializer_context(self):
+        """
+        Serializer ga o'tkaziladigan asosiy context ni qaytaradi.
+        """
+        return {
+            "request": self.request,
+            "view": self,
+            # example: AYtaylik serializerda user bilan ishlangan:
+            "user": self.request.user
+            # ...
+        }
+
+
+    # def post(self, request, *args, **kwargs):
+    #     try:
+    #         serializer = self.serializer_class(data=request.data)
+    #         if not serializer.is_valid():
+    #             print(serializer.errors)
+    #             return Response(data={"message": "Invalid data"}, status=status.HTTP_400_BAD_REQUEST)
+    #         data = serializer.validated_data
+    #         attributes = data.get('attributes', [])
+    #         subtotal = sum([AttributeValue.objects.get(id=attribute).price for attribute in attributes])
+    #         item = CartItem.objects.create(
+    #             user=request.user,
+    #             quantity=data.get("quantity"),
+    #             product=data.get("product"),
+    #             subtotal=subtotal
+    #         )
+    #         return Response(data={"message": "Product added to cart item.", "result": {"cart_item_id": item.id}})
+
+    #     except Products.DoesNotExist:
+    #         return Response(data={"message": "Product not found"}, status=status.HTTP_404_NOT_FOUND)
 
 
 class UpdateUserCartItem(UpdateAPIView):
